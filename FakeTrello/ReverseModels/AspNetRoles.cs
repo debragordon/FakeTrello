2b7e630a85fafd043f31bb6d304d942c3a98854f
Debra Gordon <debra.m.gordon@gmail.com>
2017-03-07 16:40:38 -0800

    finished in-class work

---
+++ FakeTrello/ReverseModels/AspNetRoles.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace FakeTrello.ReverseModels
+{
+    public class AspNetRoles
+    {
+        [Key]
+        [MaxLength]
+        public string Id { get; set; }
+
+        [MaxLength]
+        public string Name { get; set; }
+
+        public ICollection<AspNetUsers> Users { get; set; }
+
+    }
+}
---
+++ FakeTrello/ReverseModels/AspNetUsers.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace FakeTrello.ReverseModels
+{
+    public class AspNetUsers
+    {
+        [Key]
+        [MaxLength(128)]
+        public string Id { get; set; }
+
+        [MaxLength(128)]
+        public string Email { get; set; }
+
+        [Required]
+        public Boolean EmailConfirmed { get; set; }
+
+        public string PasswordHash { get; set; }
+
+        public string SecurityStamp { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        [Required]
+        public Boolean PhoneNumberConfirmed { get; set; }
+
+        [Required]
+        public Boolean TwoFactorEnabled { get; set; }
+
+        public DateTime LockoutEndDateUtc { get; set; }
+
+        [Required]
+        public Boolean LockoutEnabled { get; set; }
+
+        [Required]
+        public int AccessFailedCount { get; set; }
+
+        [MaxLength(256)]
+        [Required]
+        public string UserName { get; set; }
+
+        public ICollection<AspNetRoles> Roles { get; set; }
+    }
+}
+    //Id = c.String(nullable: false, maxLength: 128),
+    //EmailConfirmed = c.Boolean(nullable: false),
+    //PhoneNumberConfirmed = c.Boolean(nullable: false),
+    //TwoFactorEnabled = c.Boolean(nullable: false),
+    //LockoutEnabled = c.Boolean(nullable: false),
+    //AccessFailedCount = c.Int(nullable: false),
+    //UserName = c.String(nullable: false, maxLength: 256),
